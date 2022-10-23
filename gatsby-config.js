55b4cce244d1ea159a1ad7dfaab1bfc6e0deb409
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-10-23 19:27:57 +0500

    basic setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Gatsby Blog Site`,
+    description: `Im making this sample site with gatsby and contentful and content managment system`,
+    author: `@Mahmood`,
+    //siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
