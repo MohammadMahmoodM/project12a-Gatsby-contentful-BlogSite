dbc3c61e79c6cc4c6dd34cdb3626740efe040ea4
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-10-25 12:31:08 +0500

    third sigle page created, so that can view complete blog

---
+++ src/templates/blog-post.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import SEO from "../components/seo"
+
+export const query = graphql`
+  query {
+    contentfulBlogPost {
+        title
+        publishedDate(formatString: "Do MMMM, YYYY")
+        featuredImage {
+          gatsbyImageData
+        }
+      }
+  }
+`
+
+const BlogPost = props => {
+    return (
+        <Layout>
+            <SEO title={props.data.contentfulBlogPost.title} />
+            <Link to="/blog/">Visit the Blog Page</Link>
+            <div className="content">
+                <h1>{props.data.contentfulBlogPost.title}</h1>
+                <span className="meta">
+                    Posted on {props.data.contentfulBlogPost.publishedDate}
+                </span>
+                {props.data.contentfulBlogPost.featuredImage && (
+                    <GatsbyImage
+                        className="featured"
+                        image={getImage(props.data.contentfulBlogPost.featuredImage)}
+                        alt={props.data.contentfulBlogPost.title}
+                    />
+                )}
+            </div>
+        </Layout>
+    )
+}
+
+export default BlogPost
